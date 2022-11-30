1cc54f5227a9c30475fb48556dd5681833a29918
Lakin Wecker <lakin@wecker.ca>
2022-11-30 09:44:20 -0700

    Performance enhancements and v0.0.16

--- fairystockfish/src/main/java/org/playstrategy/FairyStockfishConfig.java
+++ fairystockfish/src/main/java/org/playstrategy/FairyStockfishConfig.java
@@ -17,7 +17,8 @@ public class FairyStockfishConfig implements InfoMapper {
         infoMap.put(new Info("fairystockfish::PieceInfo").pointerTypes("PieceInfo").define());
         infoMap.put(new Info("fairystockfish::Piece").pointerTypes("Piece").define());
         infoMap.put(new Info("std::map<std::string,fairystockfish::PieceInfo>").pointerTypes("PieceInfoMap").define());
-        infoMap.put(new Info("std::map<std::string,fairystockfish::Piece>").pointerTypes("PieceMap").define());
+        infoMap.put(new Info("std::map<std::string,fairystockfish::Piece>").pointerTypes("PieceMapUci").define());
+        infoMap.put(new Info("std::map<int,fairystockfish::Piece>").pointerTypes("PieceMap").define());
         infoMap.put(new Info("std::tuple<bool,int>").pointerTypes("BoolIntPair").define());
         infoMap.put(new Info("std::tuple<bool,bool>").pointerTypes("BoolBoolPair").define());
         infoMap.put(new Info("std::vector<std::string>").pointerTypes("VectorOfStrings").define());
